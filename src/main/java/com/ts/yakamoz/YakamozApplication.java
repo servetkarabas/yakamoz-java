b7e8a30f869d83ce5a814ac1be6aec3ca0411a37
servet.karabas <servet.karabas@servexternos.pagonxt.com>
2024-09-13 16:20:20 +0100

    first initial commit

---
+++ src/main/java/com/ts/yakamoz/YakamozApplication.java
@@ -0,0 +1,13 @@
+package com.ts.yakamoz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YakamozApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YakamozApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ts/yakamoz/YakamozApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ts.yakamoz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YakamozApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
